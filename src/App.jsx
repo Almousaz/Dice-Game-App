a7876bdfc1a1af383f058cef95cab9cca1aa89bd
Ali Mousazada <mousazada.ali@gmail.com>
2024-09-23 22:27:24 -0400

    demo two for dice game

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <div className='App'>
         <h1>Lets Design an App !</h1>
         <Dice numDice={4} maxVal={6} />
+        <Dice title='Roll Me !' />
       </div>
   )
 
--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,30 +1,30 @@
-
 import React, { useState } from "react";
-
-const Dice = ({numDice = 6 , title = "Main Game" , maxVal = 20}) => {
-
-    const [numbers , setNumbers] = useState(Array.from({length : numDice }))
-
-    const rollDice = () => (
-        setNumbers(numbers => (
-            numbers.map(n => Math.floor(Math.random() * maxVal) + 1)
-        ))
-    )
-
-    // const dice = [] ;
-
-    // for (let i = 0 ; i < numDice ; i ++) {
-    //     const num = Math.floor(Math.random() * maxVal) + 1 ;
-    //     dice.push(num)
-    // }
+import "./Dice.css"
+import Die from "./Die";
 
 
-    return (
-        <div>
-            {numbers.map(num => <div>{num}</div> )}
-            <button onClick={rollDice}>Roll</button>
-        </div>
-    )
-}
+const Dice = ({ numDice = 6, title = "Main Game", maxVal = 20 }) => {
+  const [numbers, setNumbers] = useState(Array.from({ length: numDice }));
 
-export default Dice
+  const rollDice = () =>
+    setNumbers((numbers) =>
+      numbers.map(n => Math.floor(Math.random() * maxVal) + 1)
+    );
+
+  // const dice = [] ;
+
+  // for (let i = 0 ; i < numDice ; i ++) {
+  //     const num = Math.floor(Math.random() * maxVal) + 1 ;
+  //     dice.push(num)
+  // }
+
+  return (
+    <div className="Dice">
+        <h2>{title}</h2>
+      {numbers.map(num => <Die val = {num} /> )}
+      <button onClick={rollDice}>Roll</button>
+    </div>
+  );
+};
+
+export default Dice;
